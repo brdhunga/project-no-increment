63b13fc92267eb74c6c2b7753c97b1e86f44812d
Bivu <git@git.com>
2023-04-26 22:00:57 -0600

    poetry lock updated for tailwind

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms",  # new
+    "crispy_tailwind",  # new
+
 ]
 
 MIDDLEWARE = [
